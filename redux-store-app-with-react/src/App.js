1580b023c96c234f77012ac74ab3115c2c036f5e
ashoksrt-248 <kathyayani.honey@gmail.com>
2022-07-27 15:01:18 +0530

    intial commit

---
+++ redux-store-app-with-react/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        
+        <h1>React Redux App</h1>
+        <a>
+          React Redux
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
